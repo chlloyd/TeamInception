0ff674da5f36e8b05037f51bd646908948a41d2a
Maximum Gale <kieran.gale@icloud.com>
2019-04-16 10:59:39 +0100

    Test for triggers on VR controllers

---
+++ Assets/Scripts/Reactivator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reactivator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] objs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < objs.Length; i++) objs[i].SetActive(true);
+    }
+}
---
+++ Assets/Scripts/RippleAffector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteAlways]
+public class RippleAffector : MonoBehaviour
+{
+    static public Vector4[] ripples = new Vector4[10];
+    static public int nextIndex = 0;
+    private float lastUpdateTime = 0;
+    private Material rippleMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rippleMat = gameObject.GetComponent<MeshRenderer>().material;
+        for(int i = 0; i < ripples.Length; i++) ripples[i] = Vector4.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rippleMat.SetFloat("_CurrentTime", Time.timeSinceLevelLoad);
+    }
+
+    void OnTriggerStay(Collider col) {
+        if(col.tag == "Player") {
+            if(Time.time - lastUpdateTime > 0.2f) {
+            ripples[nextIndex] = new Vector4(col.transform.position.x, col.transform.position.y, col.transform.position.z, Time.timeSinceLevelLoad);
+            nextIndex++;
+            if(nextIndex >= 10) nextIndex = 0;
+            lastUpdateTime = Time.time;
+
+            rippleMat.SetVectorArray("_RippleOrigins", ripples);
+        }
+        }
+    }
+}
