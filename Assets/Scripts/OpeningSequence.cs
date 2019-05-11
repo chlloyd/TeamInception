1e3bcbf4fd36f407beea504c337e51f00b709569
Maximum Gale <kieran.gale@icloud.com>
2019-05-11 11:59:46 +0100

    New textures/models from VFX; video projector; TeleportArea for platforms

---
+++ Assets/Scripts/OpeningSequence.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+/*
+ * OpeningSequence CLASS
+ * 
+ * Class that handles the dynamic
+ * aspects of the cinema e.g. the
+ * door that appears in the screen.
+ *
+ * -Kieran
+ */
+
+public class OpeningSequence : MonoBehaviour
+{
+    private VideoPlayer vp;
+
+    void Start() {
+        vp = gameObject.GetComponent<VideoPlayer>();
+        //This is an EventHandler. Adding a
+        //function pointer to it means that
+        //when this event occurs, the listening
+        //function (ShowDoor()) automatically runs.
+        vp.loopPointReached += ShowDoor;
+    }
+
+    void ShowDoor(VideoPlayer vp) {
+        //TODO: Display the door and its teleport point (?)
+        Debug.Log("ShowDoor()");
+    }
+}
